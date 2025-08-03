5e5736563decc17b43a83e067dadb52ebeba1395
黄兴 <huangxing@huangxingdeMacBook-Pro.local>
2025-08-03 19:54:48 +0800

    feat: room test

--- src/rooms/MyRoom.js
+++ src/rooms/MyRoom.js
@@ -7,22 +7,32 @@ exports.MyRoom = class extends colyseus.Room {
 
   onCreate (options) {
     this.onMessage("type", (client, message) => {
-       console.log("222", message);
-       this.broadcast("type", "发出" + message);
+      console.log("收到消息");
+      console.log(client.sessionId, "发送了消息", message);
+      this.broadcast("type", {
+        message: '1233'
+      });
     });
-    
+
+    this.onMessage("up", (client, message) => {
+      console.log("收到消息");
+      console.log(client.sessionId, "发送了消息", message);
+      this.broadcast("up", JSON.stringify({
+        Data: "Your message content here"
+      }));
+    });
   }
 
   onJoin (client, options) {
-    console.log(client.sessionId, "joined!");
+    console.log(client.sessionId, "已加入");
   }
 
   onLeave (client, consented) {
-    console.log(client.sessionId, "left!");
+    console.log(client.sessionId, "已离开");
   }
 
   onDispose() {
-    console.log("room", this.roomId, "disposing...");
+    console.log("room", this.roomId, "已销毁");
   }
 
 }
