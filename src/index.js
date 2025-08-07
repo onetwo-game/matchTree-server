8f4e269bf0036a868319870a2ae483b17d516771
黄兴 <huangxing@huangxingdeMacBook-Pro.local>
2025-08-07 19:13:25 +0800

    feat: 1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
-/**
- * IMPORTANT:
- * ---------
- * Do not manually edit this file if you'd like to host your server on Colyseus Cloud
- *
- * If you're self-hosting (without Colyseus Cloud), you can manually
- * instantiate a Colyseus Server as documented here:
- *
- * See: https://docs.colyseus.io/server/api/#constructor-options
- */
 const { listen } = require("@colyseus/tools");
 
 // Import arena config
